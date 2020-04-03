6ed09e8a4aa3ba4152732b4abd77657949a4e5f4
alphyfishbag <dexter2196@gmail.com>
2020-04-03 16:11:19 -0700

    Updated with Pictures of Players

--- Database.js
+++ Database.js
@@ -1,4 +1,7 @@
 function displayResults(data) {
+
+
+
 	
 
 		let first = data.name;
@@ -17,9 +20,12 @@ function displayResults(data) {
 		$("#resultTP").html(TP);
 		$("#resultTeam").html(team);
 		
-		
-		
 
+		
+		
+		
+		
+		
 		
 		
 
@@ -32,7 +38,59 @@ function submit() {
 	let url = "https://nba-players.herokuapp.com/players-stats/";
 	url = url + name2 +"/" + name;
 	$.get(url, function(data){
-	document.getElementById("raw").innerHTML = JSON.stringify(data);
+	//document.getElementById("raw").innerHTML = JSON.stringify(data);
 	 displayResults(data);
+	 
 	});
+	
+
 	}
+	function pictures()	{
+			
+			
+		$("#output").show();
+		
+		
+		let name = $("#First").val();
+		let name2 = $("#Last").val();
+		
+		let url2  = "https://nba-players.herokuapp.com/players/";
+		
+		url2 = url2 + name2 + "/" + name;
+
+		var IMG = document.getElementById("IMG");
+
+		var url = url2;
+
+		var myIMG = document.createElement('img');
+
+		myIMG.setAttribute('src', url);
+
+		IMG.appendChild(myIMG);
+
+		
+
+		
+		
+			}
+
+
+	function reload_button(){
+
+
+		location.reload();
+
+		return false;
+
+
+
+	}
+
+
+
+
+
+
+
+
+
