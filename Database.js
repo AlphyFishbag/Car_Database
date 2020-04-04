af3897906c11ca56a93ffa13953dbf3303bfca67
alphyfishbag <dexter2196@gmail.com>
2020-04-03 19:35:46 -0700

    Updated HTML and Javascript

--- Database.js
+++ Database.js
@@ -1,16 +1,24 @@
 function displayResults(data) {
+	
 
-		let first = data.first_name;
-		let last = data.last_name;
-		let height_Feet = data.height_feet;
-		let height_inch = data.height_inches;
-		let Team = data.full_name;
+		let first = data.name;
+		let team = data.team_name;
+		let games = data.games_played;
+		let Average_minutes = data.minutes_per_game; 
+		let FT = data.free_throw_percentage + "%";
+		let FG = data.field_goal_percentage + "%";
+		let TP = data.three_point_percentage + "%";
 
+		$("#resultAVG").html(Average_minutes);
+		$("#resultGames").html(games);
 		$("#resultFirst").html(first);
-		$("#resultLast").html(last);
-		$("#resultHeight_Feet").html(height_Feet);
-		$("resultHeight_Inches").html(height_inch);
-		$("resultTeam").html(team);
+		$("#resultFT").html(FT);
+		$("#resultFG").html(FG);
+		$("#resultTP").html(TP);
+		$("#resultTeam").html(team);
+		
+		
+		
 
 		
 		
@@ -19,11 +27,10 @@ function displayResults(data) {
 
 function submit() {
 	$("#output").show();
-	/* given Avengers, we want the URL to be: */
-	/* https://www.omdbapi.com/?apikey=<YOUR_KEY_HERE>&t=Avengers&y=2012 */
-	let first = $("#movieTitle").val();
-	let url = "https://www.balldontlie.io/api/v1/players/"
-	url = url + first;
+	let name = $("#First").val();
+	let name2 = $("#Last").val();
+	let url = "https://nba-players.herokuapp.com/players-stats/";
+	url = url + name2 +"/" + name;
 	$.get(url, function(data){
 	document.getElementById("raw").innerHTML = JSON.stringify(data);
 	 displayResults(data);
